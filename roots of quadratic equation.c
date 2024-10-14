cce6a4b49dfe67c9e5be03c2eafebe7fa8cda918
Malapati-mamatha-9719 <184834574+Malapati-mamatha-9719@users.noreply.github.com>
2024-10-14 12:16:37 +0530

    Create roots of quadratic equation.c
    finding the roots of quadratic equation.

---
+++ roots of quadratic equation.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int a,b,c,x,d;
+float x1,x2;
+printf("enter the value:");
+scanf("%d%d%d",&a,&b,&c);
+a*sqr(x)+b*x+c;
+d=b*b-4*a*c;
+if(d>0)
+  printf("the roots are real & distinct");
+else if(d=0)
+   printf("the roots are real & equal");
+else if(d<0)
+   printf("the roots are imaginary");
+x1=(-b+sqrt(d))/2*a;
+x2=(-b-sqrt(d))/2*a;
+printf("\n the roots of quadratic equation are %f and %f",x1,x2);
+}

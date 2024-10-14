3a77913a2fdfe51a7b560a824ca635154930fad6
Malapati-mamatha-9719 <184834574+Malapati-mamatha-9719@users.noreply.github.com>
2024-10-14 18:29:37 +0530

    Update ELECTRICITY BILL.c
    To find the total electricity bill of given units.

--- ELECTRICITY BILL.c
+++ ELECTRICITY BILL.c
@@ -1,4 +1,4 @@
-##include<stdio.h>
+#include<stdio.h>
 #include<math.h>
 void main()
 {
@@ -6,15 +6,13 @@ float units,total_bill;
 printf("enter units:");
 scanf("%f",&units);
 if(units<=50)
-total_bill=(units)*0.50
-else if(units<=50)
-total_bill=(units)*0.50
+total_bill=(units)*0.50;
 else if(units<=150)
-total_bill=(50)*0.50+(units-50)*0.75
+total_bill=(50)*0.50+(units-50)*0.75;
 else if(units<=250)
-total_bill=50*0.50+100*0.75+(units-150)*1.20
+total_bill=50*0.50+100*0.75+(units-150)*1.20;
 else if(units>250)
-total_bill=50*0.50+100*0.75+100*1.20+(units-250)*1.50
+total_bill=50*0.50+100*0.75+100*1.20+(units-250)*1.50;
 printf("the total electricity bill is %f",total_bill);
 }
   

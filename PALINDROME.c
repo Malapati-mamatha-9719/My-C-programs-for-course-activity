ee60e4fe176ae68d85af30dc942e752d740c768a
Malapati-mamatha-9719 <184834574+Malapati-mamatha-9719@users.noreply.github.com>
2024-10-14 18:23:13 +0530

    Update PALINDROME.c
    To check whether the given number is palindrome or not.

--- PALINDROME.c
+++ PALINDROME.c
@@ -4,9 +4,9 @@ void main()
      int num,rem,sum=0,m;
      printf("enter the number:");
      scanf("%d",&num);
-     for(num=m;m>0;m=m/10)
+     for(m=num;num>0;num=num/10)
        {
-         rem=m%10;
+         rem=num%10;
          sum=sum*10+rem;
        }
   if(sum==m)

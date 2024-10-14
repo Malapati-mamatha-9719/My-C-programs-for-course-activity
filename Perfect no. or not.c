daa5b1fd54f394f597fb680ef37964bb0d2addc5
Malapati-mamatha-9719 <184834574+Malapati-mamatha-9719@users.noreply.github.com>
2024-10-14 12:01:55 +0530

    Create Perfect no. or not.c
    checking perfect number or not.

---
+++ Perfect no. or not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  int n,i,sum=1;
+printf("enter the numbers:");
+scanf("%d",&n);
+for(i=2;i<=n-2;i++)
+{
+if(n%i==0)
+  sum=sum+i;
+}
+if(sum==n)
+  printf("%d is a perfect number.",n);
+ else
+   printf("%d is not a perfect number.",n);
+}
